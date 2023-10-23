c8e9a7d61c51c5dcb892bb922c98abdea0e903e3
Matteo Pucci <matteopuccifxgmail.com>
2023-10-23 16:14:22 +0200

    react first approach

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import ButtonComponent from "./components/ButtonComponent";
+import ImageComponent from "./components/ImageComponent";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ButtonComponent text="Click" />
+      <ImageComponent imageSrc="https://placekitten.com/300" />
     </div>
   );
 }
